2a7ae05cdf871b9a66a9c1ba7f8291b03e880589
unknown <BalwantKiraula>
2023-06-01 22:45:21 +0530

    optimize

--- src/components/mailbox/Inbox.js
+++ src/components/mailbox/Inbox.js
@@ -1,60 +1,41 @@
-import 'bootstrap/dist/css/bootstrap.css'
-import { useState } from 'react'
-import {useSelector,useDispatch} from 'react-redux'
+import 'bootstrap/dist/css/bootstrap.css';
+import { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { inboxActions } from '../../store/Index';
-const Inbox = ({senderEmail,subject,message,id,item,read,receiverEmail})=>{
-    const [toggle,setToggle] = useState();
-    const inboxChanging=useSelector(state=>state.inbox.idata);
-    const dispatch=useDispatch();
 
-    const openEmail = ()=>{
-        setToggle(prevState=>!toggle);
-        async function updateEmailStatus(){
-            const newItem={...item,read:true};
-            console.log(newItem)
-            const creceiverEmail=receiverEmail.replace(/[^a-zA-Z0-9]/g,'');
-            const res=await fetch(`https://mailboxclient-8985c-default-rtdb.firebaseio.com/inbox/${creceiverEmail}/${id}.json`,{
-                method:'PUT',
-                body:JSON.stringify(newItem)
-            })
-            if(res.ok){
-                const data=await res.json();
-                dispatch(inboxActions.changingInbox(data))
-            }
+const Inbox = ({ senderEmail, subject, message, id, item, read, receiverEmail, date }) => {
+    const [toggle, setToggle] = useState(true); // Modified state initialization
+    const inboxChanging = useSelector(state => state.inbox.idata);
+    const dispatch = useDispatch();
+
+    const deleteEmail = async () => {
+        const creceiverEmail = receiverEmail.replace(/[^a-zA-Z0-9]/g, '');
+        const res = await fetch(`https://mailboxclient-8985c-default-rtdb.firebaseio.com/inbox/${creceiverEmail}/${id}.json`, {
+            method: 'DELETE',
+        });
+
+        if (res.ok) {
+            const data = await res.json();
+            dispatch(inboxActions.changingInbox(data));
+            console.log("deleted");
+        } else {
+            console.log('error in deleting');
         }
-        updateEmailStatus();
     }
-    const deleteEmail = ()=>{
-        
-        async function deleteEmailStatus(){
-            
-            
-            const creceiverEmail=receiverEmail.replace(/[^a-zA-Z0-9]/g,'');
-            const res=await fetch(`https://mailboxclient-8985c-default-rtdb.firebaseio.com/inbox/${creceiverEmail}/${id}.json`,{
-                method:'DELETE',
-                
-            })
-            if(res.ok){
-                const data = await res.json();
-                dispatch(inboxActions.changingInbox(data))
-                alert("deleted")
-        }else{
-            console.log('error in deleting')
-        }
-        }
-        deleteEmailStatus();
-    }
-    return(<li className='list-group-item' >
-        <button className="btn btn-warning" onClick={openEmail}>Open</button>
-        <button className="btn btn-warning" onClick={deleteEmail}>Delete</button>
-        <div>
-            {toggle && <div>
-            <span>From:{senderEmail}</span> <br></br>
-            <span>Subject:{subject}</span> <br/>
-            <p>Message:{message}</p></div>}    
-        </div>
-            
-    </li>)
+
+    return (
+        <li className='list-group-item'>
+            <button className="btn btn-warning" onClick={deleteEmail}>Delete</button>
+            <div className={read === true ? "bg-dark" : "bg-light"}>
+                <div>
+                    <span>From: {senderEmail}</span> <br />
+                    <span>Subject: {subject}</span> <br />
+                    <p>Message: {message}</p>
+                    <span>{date}</span>
+                </div>
+            </div>
+        </li>
+    )
 }
 
 export default Inbox;
--- src/components/mailbox/Sent.js
+++ src/components/mailbox/Sent.js
@@ -1,47 +1,40 @@
 import 'bootstrap/dist/css/bootstrap.css'
 import { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { sentActions } from '../../store/Index';
 
-const Sent = ({receiverEmail,subject,message,senderEmail,item,id,read})=>{
-    const [toggle,setToggle] = useState();
-    const openEmail = ()=>{
-        setToggle(prevState=>!toggle);
-        function updateEmailStatus(){
-            const newItem={...item,read:true};
-            console.log(newItem)
-            const csenderEmail=senderEmail.replace(/[^a-zA-Z0-9]/g,'');
-            const res=fetch(`https://mailboxclient-8985c-default-rtdb.firebaseio.com/sent/${csenderEmail}/${id}.json`,{
-                method:'PUT',
-                body:JSON.stringify(newItem)
-            })
+const Sent = ({ receiverEmail, subject, message, senderEmail, item, id, read, date }) => {
+    const [toggle, setToggle] = useState(true); // Modified state initialization
+    const [color, setColor] = useState(false);
+    const sentChanging = useSelector(state => state.inbox.data);
+    const dispatch = useDispatch();
+
+    const deleteEmail = async () => {
+        const csenderEmail = senderEmail.replace(/[^a-zA-Z0-9]/g, '');
+        const res = await fetch(`https://mailboxclient-8985c-default-rtdb.firebaseio.com/sent/${csenderEmail}/${id}.json`, {
+            method: 'DELETE',
+        });
+
+        if (res.ok) {
+            const data = await res.json();
+            dispatch(sentActions.changingSent(data));
+            console.log("deletion successful");
         }
-        updateEmailStatus();
-    }
-    const deleteEmail = ()=>{
-        
-        function deleteEmailStatus(){
-            
-            
-            const csenderEmail=senderEmail.replace(/[^a-zA-Z0-9]/g,'');
-            const res=fetch(`https://mailboxclient-8985c-default-rtdb.firebaseio.com/sent/${csenderEmail}/${id}.json`,{
-                method:'DELETE',
-                
-            })
-        }
-        deleteEmailStatus();
     }
 
-
-
-
-    return(<li className='list-group-item'>
-        <button className="btn btn-warning" onClick={openEmail}>Open</button>
-        <button className="btn btn-warning" onClick={deleteEmail}>Delete</button>
-        <div>
-            {toggle && <div><span>To:{receiverEmail}</span>
-            <span>Subject:{subject}</span>
-            <p>Message:{message}</p></div>}
-        </div>
-    </li>)
+    return (
+        <li className='list-group-item'>
+            <button className="btn btn-warning" onClick={deleteEmail}>Delete</button>
+            <div className={read ? "bg-dark text-light" : "bg-light text-dark"}>
+                <div className={read ? "bg-dark text-light" : "bg-light text-dark"}>
+                    <span>To: {receiverEmail}</span><br />
+                    <span>Subject: {subject}</span><br />
+                    <p>Message: {message}</p>
+                    <span>{date}</span>
+                </div>
+            </div>
+        </li>
+    )
 }
 
 export default Sent;
